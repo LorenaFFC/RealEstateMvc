8a712953065f627d891a3722a0d175059cac0e61
LorenaFFC <fernandescunhalorena3@gmail.com>
2021-03-30 13:55:18 -0300

    ConsultantsController

---
+++ RealEstateMvc/Data/SeedingService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RealEstateMvc.Models;
+
+namespace RealEstateMvc.Data
+{
+    // REGISTRAR O SERVICO NA INJ DE DEPENDENCIA DA APP - Startup.cs
+    public class SeedingService
+    {
+        private RealEstateMvcContext _context;
+
+        public SeedingService(RealEstateMvcContext context)
+        {
+            _context = context;
+        }
+
+        // Vai Popular a base
+        public int Seed() 
+        { 
+            if(_context.Department.Any() || 
+                _context.Consultant.Any() ||
+                _context.Rent.Any())
+            {
+                return 0; // O Banc Ja foi populado
+            }
+            return 1;
+        
+        }
+
+
+    }
+}
+
